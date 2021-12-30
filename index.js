45dcd199c53f86304d74746ef0814a2b6b75149f
your_github_username <guptasumit922@gmail.com>
2021-12-30 10:40:50 +0530

    built phone backend

---
+++ index.js
@@ -0,0 +1,107 @@
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+morgan.token('body', (req,res) => JSON.stringify(req.body) );
+
+app.use(express.json());                                                                          //middleware
+app.use(morgan(':method :url :status :res[content-length] :body - :response-time ms'))
+
+                                        
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    { 
+        "id": 5,
+        "name": "Hellman Truth", 
+        "number": "39-23-6647122"
+      }
+]
+
+
+
+app.get('/',(req,res) =>{
+    //morgan(':method :url :status :res[content-length] - :response-time ms');
+    res.send(`The server is runnning on ${PORT}`) });
+
+app.get('/api/persons',(req,res) => res.json(persons))
+
+app.get('/info',(req,res) =>  { 
+    
+    let total = persons.length;
+    let date = new Date();
+    
+    res.send(`<p>Phonebook has info for ${total} people <br/><br/>  ${date}</p> ` ) } ) 
+
+app.get('/api/persons/:id',(req,res) => {
+
+    let id = Number(req.params.id);
+    let individual = persons.find( person => person.id === id );
+
+    if(individual === undefined)
+        res.status('404').end();
+
+    res.json(individual);
+
+})
+
+
+app.delete('/api/persons/:id',(req,res) => {
+
+    let id =  Number(req.params.id);
+    persons = persons.filter( (person) => person.id !== id  );
+    res.status(204).end();
+
+})
+
+
+app.post('/api/persons',(req,res) => {
+    let newPerson = req.body;
+
+    if(!newPerson.name || !newPerson.number){
+        return res.status(400).json({
+            "error":"contents are misssing"
+        })
+
+    }
+
+    if(persons.find( person => person.name === newPerson.name )){
+        return res.status(400).json({
+            "error":"name must be unique"
+        })
+    }
+
+    newPerson["id"] = Math.floor((Math.random() * 9876543) + 12345);
+    persons.push(newPerson);
+    res.send(newPerson);
+
+})
+
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`The server is listening to ${PORT}`)
+
+
+
